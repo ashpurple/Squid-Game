f09d2aeec19977c8c931f0dd5f1c5dbebf613676
HoJun Choi <nyhj962@gmail.com>
2021-11-08 22:56:57 +0900

    Update map.js

--- js/camera.js
+++ js/camera.js
@@ -4,7 +4,7 @@ import { PerspectiveCamera } from '../node_modules/three/src/cameras/Perspective
 function createCamera() {
   const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-  camera.position.set(0, 3, 6);
+  camera.position.set(0, 3, 8);
   // camera.
   // camera.position.z = 5;
   // camera.rotation.y = 0;
--- js/scene.js
+++ js/scene.js
@@ -2,9 +2,11 @@ import {Scene} from '../node_modules/three/src/scenes/Scene.js'
 import {Color} from '../node_modules/three/src/math/Color.js'
 import {createRoom} from './map.js'
 
+const SkyColor = "#afe8f7"
+
 function createScene() {
   const scene = new Scene();
-  scene.background = new Color('skyblue');
+  scene.background = new Color(SkyColor);
 
   createRoom(scene);
 
